19b10b35cf5750fc1a5b41e687c37278347c5990
Manvir Dhillon <Manvir5@csu.fullerton.edu>
2025-10-02 21:22:26 -0700

    new frontend integration

---
+++ src/WebPages/HomePage.jsx
@@ -0,0 +1,12 @@
+function HomePage() {
+    return(
+    <div className="flex items-center justify-center-safe h-screen z-100">
+    Home Lorem ipsum dolor sit amet consectetur 
+    adipisicing elit. Blanditiis itaque possimus odio unde maiores. 
+    Praesentium officia iste corrupti, earum corporis reiciendis quidem error, 
+    officiis tempore quis incidunt deserunt veritatis. Aliquam?
+    </div>
+    )
+}
+
+export default HomePage
